1dc9f6f97d84649631f07991a040d650ac088796
Alex Delgado <delgadoalex@icloud.com>
2018-08-09 13:37:00 -0500

    oauth changed

---
+++ oauth_server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const request = require('request')
+const querystring = require('querystring')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const redirect_uri = 
+  process.env.REDIRECT_URI || 
+  'http://localhost:9000/callback'
+
+app.get('/login', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
+})
+
+app.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    console.log(body);
+    // to do: change to req.body.access
+    let access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+})
+
+const port = process.env.PORT || 9000
+console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
+app.listen(port)
